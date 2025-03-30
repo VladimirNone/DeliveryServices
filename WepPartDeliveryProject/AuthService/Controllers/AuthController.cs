fc11b7ffd6716e3fbde754f4633d875b4131a193
VladimirSharp <zhiltsov.v.a@yandex.ru>
2025-03-30 17:46:57 +0300

    Добавил авторизационный сервис

---
+++ WepPartDeliveryProject/AuthService/Controllers/AuthController.cs
@@ -0,0 +1,134 @@
+﻿using DbManager.Data;
+using DbManager.Data.DTOs;
+using DbManager.Data.Nodes;
+using DbManager.Neo4j.Interfaces;
+using DbManager.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WepPartDeliveryProject.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IRepositoryFactory _repositoryFactory;
+        private readonly IPasswordService _pswService;
+        private readonly JwtService _jwtService;
+
+        public AuthController(IRepositoryFactory repositoryFactory, IPasswordService passwordService, JwtService jwtService)
+        {
+            _repositoryFactory = repositoryFactory;
+            _pswService = passwordService;
+            _jwtService = jwtService;
+        }
+
+        private void AddCookieDataToResponse(string RefreshToken, string? userId = null)
+        {
+            if(userId != null) 
+            {
+                Response.Cookies.Append("X-UserId", userId,
+                            new CookieOptions() { HttpOnly = true, Secure = true, Expires = DateTime.Now.AddDays(30), SameSite = SameSiteMode.None });
+            }
+
+            Response.Cookies.Append("X-Refresh-Token", RefreshToken,
+                        new CookieOptions() { HttpOnly = true, Secure = true, Expires = DateTime.Now.AddDays(30), SameSite = SameSiteMode.None });
+
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(UserLoginInDTO data)
+        {
+            var users = await _repositoryFactory.GetRepository<User>().GetNodesByPropertyAsync("Login", new[] { data.Login });
+            if(users.Count != 1)
+            {
+                return BadRequest("Вы ввели не верный логин");
+            }
+
+            var user = users[0];
+
+            if (user.IsBlocked)
+            {
+                return BadRequest("Ваш аккаунт был заблокирован!");
+            }
+
+            if(_pswService.CheckPassword(data.Login, data.Password, user.PasswordHash.ToArray()))
+            {
+                user.RefreshToken = Guid.NewGuid();
+                user.RefreshTokenCreated = DateTime.Now;
+
+                await ((IUserRepository)_repositoryFactory.GetRepository<User>()).UpdateRefreshTokenAsync(user);
+
+                AddCookieDataToResponse(user.RefreshToken.ToString(), user.Id.ToString());
+
+                return Ok();
+            }
+
+            return Unauthorized("Вы ввели не верный логин или пароль");
+        }
+
+        [HttpPost("refreshAccessToken")]
+        public async Task<IActionResult> RefreshAccessToken()
+        {
+            var userRepo = (IUserRepository)_repositoryFactory.GetRepository<User>();
+
+            var userId = Request.Cookies["X-UserId"];
+            if (userId == null)
+                return BadRequest("У вас отсутсвует refresh token. Вам необходимо авторизоваться или зарегистрироваться.");
+
+            var inputRefreshToken = Request.Cookies["X-Refresh-Token"];
+
+            var userNode = await userRepo.GetNodeAsync(userId);
+
+            if (userNode.IsBlocked)
+                return BadRequest("Ваш аккаунт был заблокирован!");
+
+            if (inputRefreshToken == userNode.RefreshToken.ToString() && userNode.RefreshTokenCreated.AddDays(60) > DateTime.Now)
+            {
+                var userRoles = await userRepo.GetUserRoles(userId);
+
+                var jwtTokenInfo = _jwtService.GenerateAccessJwtToken(userId, userRoles);
+
+                AddCookieDataToResponse(userNode.RefreshToken.ToString());
+                
+                return Ok(jwtTokenInfo);
+            }
+
+            return BadRequest("You refresh token don't work. You need to login or signup to system");
+        }
+
+        [HttpPost("signup")]
+        public async Task<IActionResult> Signup(UserSignupInDTO data)
+        {
+            var user = new User() { 
+                Login = data.Login, 
+                Name = data.Name,
+                Born = data.Born,
+                Address = data.Address,
+                PhoneNumber = data.PhoneNumber,
+                RefreshToken = Guid.NewGuid(), 
+                RefreshTokenCreated = DateTime.Now, 
+                PasswordHash = _pswService.GetPasswordHash(data.Login, data.Password).ToList(), 
+            };
+
+            await _repositoryFactory.GetRepository<User>().AddNodeAsync(user);
+            await _repositoryFactory.GetRepository<User>().SetNewNodeType<Client>(user.Id.ToString());
+
+            AddCookieDataToResponse(user.RefreshToken.ToString(), user.Id.ToString());
+
+            return Ok();
+        }
+
+        [Authorize]
+        [HttpPost("logout")]
+        public async Task<IActionResult> Logout()
+        {
+            Response.Cookies.Delete("X-UserId",
+                        new CookieOptions() { HttpOnly = true, Secure = true, Expires = DateTime.Now.AddDays(60), SameSite = SameSiteMode.None });
+            Response.Cookies.Delete("X-Refresh-Token",
+                        new CookieOptions() { HttpOnly = true, Secure = true, Expires = DateTime.Now.AddDays(60), SameSite = SameSiteMode.None });
+
+            return await Task.FromResult(Ok());
+        }
+    }
+}
